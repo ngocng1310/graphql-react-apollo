ee523e68d7cab6f89e4f166defc27e1ce5bb20e8
ngocng <ngoc.nguyen1@gbst.com>
2018-12-18 12:07:19 +1000

    tweaks

---
+++ public/precache-manifest.3ca0abf4f6885eecb0f30edafa463e0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d39d5022fa23faa67170144207847eae",
+    "url": "/static/media/logo.d39d5022.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ee59e4b0fd264f2912a",
+    "url": "/static/js/main.1ee59e4b.chunk.js"
+  },
+  {
+    "revision": "3fadb6410cf5793da420",
+    "url": "/static/js/1.3fadb641.chunk.js"
+  },
+  {
+    "revision": "1ee59e4b0fd264f2912a",
+    "url": "/static/css/main.b875c2ab.chunk.css"
+  },
+  {
+    "revision": "1f7c45e70b5634b99f6951461048c497",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -2,29 +2,26 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
 const schema = require('./schema');
+const path = require('path');
 
 const app = express();
 
 // Allow cross-origin
 app.use(cors());
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+    '/graphql', 
+    graphqlHTTP({
     schema,
     graphiql: true
 }));
 
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/public'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
